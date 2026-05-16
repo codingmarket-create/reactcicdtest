53b71b39a255c08e3597ec38bd8b9993ff1343b1
Rajeshdn <coding.market@gmail.com>
2026-05-16 03:35:05 -0700

    CI CD pipeline setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Welcome to React</h1>
+      <p>This is a simple React application to test ci cd pipeline.</p>
+    </div>
+  )
+}
+
+export default App
